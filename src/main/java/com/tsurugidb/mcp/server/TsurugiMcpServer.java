e05b2c591c46e842b3e641d4affe7684fee645b0
Hishida Masato <hishida@nautilus-technologies.com>
2025-05-22 08:23:16 +0900

    Change version to 0.2.0-SNAPSHOT

--- src/main/java/com/tsurugidb/mcp/server/TsurugiMcpServer.java
+++ src/main/java/com/tsurugidb/mcp/server/TsurugiMcpServer.java
@@ -26,7 +26,7 @@ import io.modelcontextprotocol.spec.McpServerTransportProvider;
 public class TsurugiMcpServer {
 
     public static final String SERVER_NAME = "tsurugi-mcp-server";
-    public static final String SERVER_VERSION = "0.1.0";
+    public static final String SERVER_VERSION = "0.2.0-SNAPSHOT";
 
     public static McpSyncServer syncServer(McpServerTransportProvider transportProvider, ObjectMapper objectMapper, Arguments arguments, SessionPool pool) {
         var tools = TsurugiMcpTool.syncTools(objectMapper, arguments, pool);
